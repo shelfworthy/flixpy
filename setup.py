489ea58d0260045a7fecc2868f8ccd9b6cba5dd4
Chris Drackett <chris@drackett.com>
2012-10-10 19:13:31 -0500

    finishing up name change.

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 from setuptools import setup, find_packages
 
 version = '0.1'
@@ -6,10 +9,13 @@ setup(
     name='python-netflix-streaming',
     version=version,
     description="A python API wrapper specificly designed to work with netflix's streaming API",
+    long_description=read(os.path.join(os.path.dirname(__file__), 'README.markdown')),
     author='Chris Drackett',
     author_email='chris@shelfworthy.com',
-    url='http://github.com/shelfworthy/python-textile',
+    url='http://github.com/shelfworthy/python-netflix-streaming',
     packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
@@ -20,6 +26,4 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     keywords='api,netflix,streaming',
-    include_package_data=True,
-    zip_safe=False,
 )
