840ffc0a31c7a6191c505ceb4bd386f3185a44e3
Chris Drackett <chris@drackett.com>
2013-04-30 20:21:12 -0700

    update search and autocomplete to more correctly match API.

--- flixpy/catalog.py
+++ flixpy/catalog.py
@@ -8,10 +8,23 @@ class NetflixCatalog(object):
         # NOTE this downloads *all* the streaming titles on netflix. This may take a while ;)
         return self.client.get_resource('/catalog/titles/streaming', *args, **kwargs)
 
-    def _search(self, url, term, startIndex=None, maxResults=None, expand=None, show_disks=False):
-        parameters = {
-            'term': term,
-        }
+    def _search(self, url, term, expand=None, parameters=None):
+        if not parameters:
+            parameters = {}
+
+        parameters['term'] = term
+
+        return self.client.get_resource(url, parameters, expand=expand)
+
+    def autocomplete(self, term):
+        results = self._search('/catalog/titles/autocomplete', term)
+
+        if results['autocomplete']:
+            return results['autocomplete']['title']
+        return []
+
+    def search(self, term, startIndex=None, maxResults=None, expand=None, show_disks=False):
+        parameters = {}
 
         if not show_disks:
             parameters['filters'] = '%s/categories/title_formats/instant' % self.client.server
@@ -20,17 +33,7 @@ class NetflixCatalog(object):
         if maxResults:
             parameters['max_results'] = maxResults
 
-        return self.client.get_resource(url, parameters, expand=expand)
-
-    def autocomplete(self, term, startIndex=None, maxResults=None):
-        results = self._search('/catalog/titles/autocomplete', term, startIndex, maxResults)
-
-        if results['autocomplete']:
-            return results['autocomplete']['title']
-        return []
-
-    def search(self, term, startIndex=None, maxResults=None, expand=None):
-        results = self._search('/catalog/titles', term, startIndex, maxResults, expand)
+        results = self._search('/catalog/titles', term, expand, parameters)
 
         try:
             return [NetflixTitle(title, self.client) for title in results['catalog']]
